02e71c70781b771309636f7fcd1a77833ca0f4e9
fzs09 <aiadnan1993@gmail.com>
2016-12-29 11:05:34 +0600

    Primary Session class is done

--- site/lib/session.php
+++ site/lib/session.php
@@ -5,11 +5,11 @@
 * This session handler class handles all the session related work.
   Most Important usage is in setting session. and getting the user ID.
 */
-class session
+final class session
 {
 	public static $user_id;
 	public static $user_name;
-
+	private $session_vars;
 	//$user_id, $user_name=null
 	
 	function __construct(){
@@ -17,14 +17,32 @@ class session
 		if (session_status() == PHP_SESSION_NONE) {
    			 session_start();
 		}
+		else if(isset($_SESSION['id']) && isset($_SESSION['user_name'])){
+			$this->_decrypt_user();
+		}
 
 
 	}// End of constructor function
 
 
-	private function setSession(){
+	public function set_session($session_name, $session_value){
+		$_SESSION[$session_name]=$session_value;
+		$this->session_vars[$session_name]=$session_value;
 
+	}// End of function setSession
 
+	public function get_session($session_name){
+		if(isset($_SESSION[$session_name])){
+
+			return $_SESSION[$session_name];
+		}
+		return null;
+
+	}// End of 
+
+	private function _decrypt_user(){
+		self::$user_name=base64_decode($_SESSION['id']);
+		self::$user_id= base64_decode($_SESSION['user_name']);
 	}
 
 	public function set_credential_session($user_name, $user_id){
@@ -32,23 +50,23 @@ class session
 
 			return FALSE;
 		}else{
+			self::$user_name=$user_name; 
+			self::$user_id=$user_id;
 			$_SESSION['id']=base64_encode($user_id);
-			echo $_SESSION['id'];
-			$_SESSION['user_name']=base64_encode();
+			
+			$_SESSION['user_name']=base64_encode($user_name);
 			return true;
 
 		}
 	}// End of function set_credential_session
 
-	public function 
+	
 
 
 
 
 }// End of class session
 
-$session = new session();
-$session->set_credential_session("koushikjay66", "13101206");
-echo "<br>";
-$session = new session()
+ $session = new session();
+
 

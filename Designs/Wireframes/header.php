7528c2b75e6dab7bb2303e3b9f66e2678212f963
Koushik Jay <Koushik Jay>
2017-02-22 10:58:38 +0600

    Wireframing is done . Now started working on wireframed HTMLs

---
+++ Designs/Wireframes/header.php
@@ -0,0 +1,15 @@
+<!-- This is the header tag-->
+
+<div id="header_section" class="header">
+
+	<div class="search_bar_Section" id="search_bar_Section">
+		
+	</div>
+	<!-- End of div Search Bar-->
+	<div id="notification_section">
+		<img src="" id="notification_img" class="notification">
+		<img src="" id="notification_count" >
+	</div>
+	<!-- End of notification Section -->
+
+</div>
